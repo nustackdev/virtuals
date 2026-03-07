cf65ba4016ace4b873bd9283e78341d61de28862
Gor <arakelyangor10@gmail.com>
2026-03-07 22:17:40 +0400

    Fix import issues in views module

--- src/virtuals/views/__init__.py
+++ src/virtuals/views/__init__.py
@@ -2,16 +2,21 @@
 
 from __future__ import annotations
 
-from .bytearray_view import ByteArrayView
-from .dict_view import DictISliceView, DictView
-from .flat_dict_view import FlatDictView
-from .flat_list_view import FlatListView
-from .frozenset_view import FrozenSetView
-from .indexed_dict_view import IndexedDictSliceView, IndexedDictView
-from .light_dict_view import LightDictView
-from .list_view import ListSliceView, ListView
-from .set_view import SetView
-from .tuple_view import TupleView
+from virtuals.views import (
+    ByteArrayView,
+    DictISliceView,
+    DictView,
+    FlatDictView,
+    FlatListView,
+    FrozenSetView,
+    IndexedDictSliceView,
+    IndexedDictView,
+    LightDictView,
+    ListSliceView,
+    ListView,
+    SetView,
+    TupleView,
+)
 
 
 __all__ = (
